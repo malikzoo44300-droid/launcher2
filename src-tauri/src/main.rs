9055aa021e111e81d4401d38e4bafc97371532fd
malikzoo44300-droid <malikzoo44300@gmail.com>
2026-06-08 00:10:55 +0000

    Finalisation corrections build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,12 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[tokio::main]
 async fn main() {
-    retrac_launcher_lib::run(vec![]);
-}
-fn main() {
-  // Ajoute cette ligne avant le builder
-  std::env::set_var("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--disable-gpu --disable-software-rasterizer");
-
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    // 1. Définition des variables d'environnement
+    std::env::set_var("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--disable-gpu --disable-software-rasterizer");
+    
+    // 2. Récupération des arguments et appel de la bibliothèque
+    let args: Vec<String> = std::env::args().collect();
+    retrac_launcher_lib::run(args);
+    
 }
